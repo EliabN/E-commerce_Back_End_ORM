fd8add54cc20af611c766a8e825be08c925a420e
eliabn <30064396@tafe.wa.edu.au>
2023-09-10 19:02:34 +0930

    Udated Category.js/Product.js and added TODOs

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+// Create a new Sequelize model for category
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    //TODO:define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// TODO:Products belongsTo Category
 
 // Categories have many Products
 
