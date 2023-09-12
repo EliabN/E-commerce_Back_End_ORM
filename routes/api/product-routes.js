76b174f6bfb56ce66ac2dcb60fce64e8b42e2acf
eliabn <30064396@tafe.wa.edu.au>
2023-09-12 18:11:10 +0930

    Updated routes/api/product-routes.js all routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// create new product
+// POST Create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -116,6 +116,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedProduct) => {
+      res.json(deletedProduct);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
